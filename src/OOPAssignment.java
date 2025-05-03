370657f54a22f6c4c3f4e86a068443e6561bba8b
seonghoon <seonghoon315@naver.com>
2025-05-03 23:58:51 +0900

    third commit

--- src/OOPAssignment.java
+++ src/OOPAssignment.java
@@ -50,19 +50,37 @@ public class OOPAssignment {
         }
         //딜러는 플레이어의 카드를 평가하고 결과를 점수로 반환한다.(점수가 높을 수록 좋음)
         //카드의 평가는 일반적인 포커의 랭크를 참고하여 높은 랭크에게 더 높은 점수를 준다.
+        int[] score = new int[playernum];
         int higherscore = 0;
-        String winner = "";
+        int winner = -1; // 위너의 디폴트 값은 -1, 플레이어[i]가 0부터 시작되고, 승자의 번호를 winner에 저장하므로 무승부일때는 기본값인 -1이다.
+        boolean isDraw = false;
+
         for (int i = 0; i < playernum; i++) {
-            int score = d.calculate(player[i]);
-            System.out.printf("%s의 점수: %d%n", player[i].nickname, score);
-            if (score > higherscore) {
-                higherscore = score;
-                winner = player[i].nickname;
-            } else if (score == higherscore) {
-                System.out.println("무승부");
+            score[i] = d.calculate(player[i]);
+            System.out.printf("%s의 점수: %d%n", player[i].nickname, score[i]);
+            if (score[i] > higherscore) {
+                higherscore = score[i];
+                winner = i;
+                isDraw = false;
+            } else if (score[i] == higherscore) {
+                isDraw = true;
             }
         }
-        System.out.printf("승자: %s%n", winner);
+        //승무패 조건식
+        if (isDraw) {
+            System.out.println("승자: 무승부로 승자 없음");
+        } else {
+            player[winner].wincount++;
+            player[winner].money += 100;
+            System.out.printf("승자: %s%n", player[winner].nickname);
+
+            for (int i = 0; i < playernum; i++) {
+                if (i != winner) {
+                    player[i].losecount++;
+                }
+            }
+        }
+
         //매 게임마다 딜러는 각 플레이어의 카드를 평가하여 결과를 출력한다.
         //게임에서 승리한 플레이어는 상금 100원과 1승이 추가되고 나머지 플레이어는 상금 0원과 1패가 추가된다.
         //100번의 게임을 자동적으로 반복해서 실행하여 최종 결과를 승리의 수가 많은 플레이어부터 내림차순으로 정렬하여 화면에 출력한다.
@@ -116,7 +134,7 @@ class Dealer {
                 && players.playerdeck[i].kind == players.playerdeck[i + 3].kind
                 && players.playerdeck[i].kind == players.playerdeck[i + 4].kind;
 
-        //로얄 스트레이트 플러시=13점
+        //로얄 스트레이트 플러시=13점: 카드 5장이 모두 무늬가 같고, 10,J,Q,K,A로 이루어진 패
         if (samekind
                 && sortingdeck[i] == 1
                 && sortingdeck[i + 1] == 10
@@ -125,7 +143,7 @@ class Dealer {
                 && sortingdeck[i + 4] == 13) {
             return 13;
         }
-        //백스트레이트 플러시=12점
+        //백스트레이트 플러시=12점: 카드 5장이 모두 무늬가 같고, A,2,3,4,5로 이루어진 패 
         else if (samekind
                 && sortingdeck[i] == 1
                 && sortingdeck[i + 1] == 2
@@ -134,7 +152,7 @@ class Dealer {
                 && sortingdeck[i + 4] == 5) {
             return 12;
         }
-        //스트레이트 플러시=11점
+        //스트레이트 플러시=11점: 카드 5장이 모두 무늬가 같고, 숫자가 연속적으로 이루어진 패
         else if (samekind
                 && ((sortingdeck[i + 1] - sortingdeck[i] == 1) && (sortingdeck[i] != 1))
                 && (sortingdeck[i + 2] - sortingdeck[i + 1] == 1)
@@ -142,7 +160,7 @@ class Dealer {
                 && (sortingdeck[i + 4] - sortingdeck[i + 3] == 1)) {
             return 11;
         }
-        //포카드=10점
+        //포카드=10점: 같은 숫자의 카드 4장으로 이루어진 패
         else if ((sortingdeck[i] == sortingdeck[i + 1]
                 && sortingdeck[i] == sortingdeck[i + 2]
                 && sortingdeck[i] == sortingdeck[i + 3])
@@ -152,7 +170,7 @@ class Dealer {
                 && sortingdeck[i + 1] == sortingdeck[i + 4])) {
             return 10;
         }
-        //풀하우스=9점
+        //풀하우스=9점: 같은 숫자 3개(트리플)와 같은 숫자 2개(원페어)로 이루어진 패
         else if ((sortingdeck[i] == sortingdeck[i + 1]
                 && sortingdeck[i] == sortingdeck[i + 2]
                 && sortingdeck[i + 3] == sortingdeck[i + 4])
@@ -162,11 +180,11 @@ class Dealer {
                 && sortingdeck[i + 2] == sortingdeck[i + 4])) {
             return 9;
         }
-        //플러시=8점
+        //플러시=8점: 카드 5장이 모두 무늬가 같은 패
         else if (samekind) {
             return 8;
         }
-        //마운틴=7점
+        //마운틴=7점: 모든 무늬가 같지는 않고, 10,J,Q,K,A로 이루어진 패
         else if (sortingdeck[i] == 1
                 && sortingdeck[i + 1] == 10
                 && sortingdeck[i + 2] == 11
@@ -174,7 +192,7 @@ class Dealer {
                 && sortingdeck[i + 4] == 13) {
             return 7;
         }
-        //백스트레이트=6점
+        //백스트레이트=6점: 모든 무늬가 같지는 않고, A,2,3,4,5로 이루어진 패
         else if (sortingdeck[i] == 1
                 && sortingdeck[i + 1] == 2
                 && sortingdeck[i + 2] == 3
@@ -182,14 +200,14 @@ class Dealer {
                 && sortingdeck[i + 4] == 5) {
             return 6;
         }
-        //스트레이트=5점
+        //스트레이트=5점: 모든 무늬가 같지는 않고, 숫자가 연속적으로 이루어진 패
         else if ((sortingdeck[i + 1] - sortingdeck[i] == 1)
                 && (sortingdeck[i + 2] - sortingdeck[i + 1] == 1)
                 && (sortingdeck[i + 3] - sortingdeck[i + 2] == 1)
                 && (sortingdeck[i + 4] - sortingdeck[i + 3] == 1)) {
             return 5;
         }
-        //트리플=4점
+        //트리플=4점: 5장의 카드 중에서 3장의 숫자가 같은 패
         else if ((sortingdeck[i] == sortingdeck[i + 1]
                 && sortingdeck[i] == sortingdeck[i + 2])
                 ||
@@ -200,7 +218,7 @@ class Dealer {
                 && sortingdeck[i + 2] == sortingdeck[i + 4])) {
             return 4;
         }
-        //투페어=3점
+        //투페어=3점: 같은 숫자 두개(원페어)가 두 쌍이 있는 패
         else if ((sortingdeck[i] == sortingdeck[i + 1]
                 && sortingdeck[i + 2] == sortingdeck[i + 3])
                 ||
@@ -211,7 +229,7 @@ class Dealer {
                 && sortingdeck[i + 3] == sortingdeck[i + 4])) {
             return 3;
         }
-        //원페어=2점
+        //원페어=2점: 같은 숫자 두개가 한 쌍이 있는 패
         else if ((sortingdeck[i] == sortingdeck[i + 1])
                 ||
                 (sortingdeck[i + 1] == sortingdeck[i + 2])
@@ -221,7 +239,7 @@ class Dealer {
                 (sortingdeck[i + 3] == sortingdeck[i + 4])) {
             return 2;
         }
-        //노페어=1점
+        //노페어=1점: 어떤 경우에도 해당하지 않는 패
         else {
             return 1;
         }
@@ -257,7 +275,7 @@ class Card {
 
 class Player {
     String nickname;
-    final int money = 10000;
+    int money = 10000;
     int wincount;
     int losecount;
     Card[] playerdeck = new Card[5];
